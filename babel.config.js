021e4dc650bd6a4db06b40b9939088f4a0051683
mohamedsahal <mohaansahal@gmail.com>
2025-09-13 10:46:43 +0300

    fixed

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,23 @@
 module.exports = function (api) {
   api.cache(true);
 
+  const plugins = [
+    // Worklets plugin must be last
+    'react-native-worklets/plugin',
+  ];
+
+  // Add production plugins if available
+  if (process.env.NODE_ENV === 'production') {
+    try {
+      require.resolve('babel-plugin-transform-remove-console');
+      plugins.push(['babel-plugin-transform-remove-console', { exclude: ['error'] }]);
+    } catch (error) {
+      console.warn('babel-plugin-transform-remove-console not found, skipping console removal');
+    }
+  }
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // Worklets plugin must be last
-      'react-native-worklets/plugin',
-      // Production plugins
-      ...(process.env.NODE_ENV === 'production' 
-        ? [['transform-remove-console', { exclude: ['error'] }]]
-        : []
-      ),
-    ],
+    plugins,
   };
 };
